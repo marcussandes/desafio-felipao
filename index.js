93a206fc22c30447770210fd77c4dca95c747306
marcussandes <marcus.sandesf@gmail.com>
2024-10-04 11:58:57 -0300

    desafio concluido

--- index.js
+++ index.js
@@ -28,12 +28,27 @@ Ao final deve se exibir uma mensagem:
 
 //Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói
 const nomeHeroi = "Marcus Sandes"
-let xpHeroi = 1010
+let xpHeroi = 1500
 
-let nivelHeroi
+    if (xpHeroi <= 1000){
+    console.log("O herói " + nomeHeroi + " está no nivel ferro")
+    }
+    else if (xpHeroi >1001 & xpHeroi <=2000)
+        nivel = "bronze"
+    else if (xpHeroi >2001 & xpHeroi <=5000)
+        nivel = "Prata"
+    else if (xpHeroi >5001 & xpHeroi <=7000)
+        nivel = "Ouro"
+    else if (xpHeroi >7001 & xpHeroi <=8000)
+        nivel = "Platina"
+    else if (xpHeroi >8001 & xpHeroi <=9000)
+        nivel = "Ascendente"
+    else if (xpHeroi >9001 & xpHeroi <=1000)
+        nivel = "Imortal"
+    else 
+        nivel = "Radiante"
+    console.log(`O herói ${nomeHeroi} está no nível  ${nivel}`)
+   
 
-do {
-    nivelHeroi = prompt('Digite um numero de 0 a 100000')
-} while (nivelHeroi >= 1000 || nivelHeroi <= 2000 )
-    console.log
 
+    
